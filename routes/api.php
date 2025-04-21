618626fa2c6a1bf7ea4a07342fa3d71e07d75970
Shahrukh Khan <shah353250@gmail.com>
2025-04-22 00:06:29 +0500

    Rate limiter implemented for security.

--- routes/api.php
+++ routes/api.php
@@ -3,11 +3,11 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ExportTranslationsController;
 use App\Http\Controllers\TranslationsController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+
 Route::group(['prefix' => 'auth'], function () {
-  Route::post('login', [AuthController::class, 'login']);
+  Route::post('login', [AuthController::class, 'login'])->middleware('throttle:5,1');
 
   Route::group(['middleware' => 'auth:sanctum'], function () {
     Route::get('logout', [AuthController::class, 'logout']);
@@ -15,6 +15,6 @@ Route::group(['prefix' => 'auth'], function () {
 });
 
 Route::group(['middleware' => 'auth:sanctum'], function () {
-  Route::apiResource('translations', TranslationsController::class);
-  Route::get('export/translations/{locale?}', [ExportTranslationsController::class, 'export']);
+  Route::apiResource('translations', TranslationsController::class)->middleware('throttle:50,1');
+  Route::get('export/translations/{locale?}', [ExportTranslationsController::class, 'export'])->middleware('throttle:50,1');;
 });
